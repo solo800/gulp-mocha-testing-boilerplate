4d7bea7c7c1f9d1966f3f946821263345263dfe2
Adam <adameliot1@gmail.com>
2018-01-09 14:44:33 -0800

    Revert "Initial commit"
    This reverts commit 11902e9352298fed0b4bf5e4678ab3fb325d2cd9.

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
-
-gulp.task('process:javascript-tests', () => {
-    gulp.src('test.js', {read: false})
-        .pipe(mocha({recursive: true}));
-});
-
-gulp.task('default', ['process:javascript-tests']);
